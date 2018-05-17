6d8496ffe5f74fa47ae1031e14e15eca4cdcff1c
quiggy <avessey@lsa2.com>
2018-05-17 14:25:04 -0400

    Added more methods to Qlib.Dict as well as a number of classes.

--- src/Dict.hs
+++ src/Dict.hs
@@ -1,18 +1,50 @@
+--a bad, not fast dictionary
+
 module Qlib.Dict where
 
-import Data.Maybe
+import qualified Data.Set as Set
 
 data Dict k v = Empty | Entry k v (Dict k v) deriving (Show)
+
+instance Foldable (Dict k) where
+    foldr _ b Empty = b
+    foldr f b (Entry k a next) = foldr f (f a b) next
 
 instance Functor (Dict k) where
     fmap _ Empty = Empty
     fmap f (Entry k v next) = Entry k (f v) (fmap f next)
 
-insert :: (Eq k) => k -> v -> Dict k v -> Dict k v
-insert k v Empty = Entry k v Empty
-insert k v (Entry nk nv next)
-    | k == nk = Entry k v next
-    | otherwise = Entry nk nv (insert k v next)
+instance Monoid (Dict k v) where
+    mempty = Empty
+
+instance Semigroup (Dict k v) where
+    (<>) x Empty = x
+    (<>) Empty y = y
+    (<>) (Entry k v next) y = Entry k v $ next <> y
+
+instance Traversable (Dict k) where
+    traverse f Empty = pure Empty
+    traverse f (Entry k v next) = Entry k <$> f v <*> traverse f next
+
+elems :: Dict k v -> [v]
+elems Empty = []
+elems (Entry _ v next) = v : elems next
+
+filterElems :: (v -> Bool) -> Dict k v -> Dict k v
+filterElems _ Empty = Empty
+filterElems f (Entry k v next)
+    | f v = Entry k v (filterElems f next)
+    | otherwise = filterElems f next
+
+filterKeys :: (k -> Bool) -> Dict k v -> Dict k v
+filterKeys _ Empty = Empty
+filterKeys f (Entry k v next)
+    | f k = Entry k v (filterKeys f next)
+    | otherwise = filterKeys f next
+
+fromList :: (Eq k) => [(k, v)] -> Dict k v
+fromList [] = Empty
+fromList (x:xs) = insert (fst x) (snd x) (fromList xs)
 
 get :: (Eq k) => k -> Dict k v -> Maybe v
 get _ Empty = Nothing
@@ -20,9 +52,48 @@ get k (Entry nk v next)
     | k == nk = Just v
     | otherwise = get k next
 
+insert :: (Eq k) => k -> v -> Dict k v -> Dict k v
+insert k v Empty = Entry k v Empty
+insert k v (Entry nk nv next)
+    | k == nk = Entry k v next
+    | otherwise = Entry nk nv (insert k v next)
+
+keys :: Dict k v -> [k]
+keys Empty = []
+keys (Entry k _ next) = k : keys next
+
 singleton :: (Eq k) => k -> v -> Dict k v
 singleton k v = Entry k v Empty
+
+size :: Dict k v -> Int
+size Empty = 0
+size (Entry _ _ next) = 1 + size next
+
+sortKeys :: (Ord k) => Dict k v -> Dict k v
+sortKeys Empty = Empty
+sortKeys (Entry k v next) = lesser <> greater
+    where
+        lesser = filterKeys (<=k) (Entry k v next)
+        greater = filterKeys (>k) next
 
 toList :: Dict k v -> [(k, v)]
 toList Empty = []
 toList (Entry k v next) = (k, v) : toList next
+
+--left-preferring
+union :: (Eq k) => Dict k v -> Dict k v -> Dict k v
+union x Empty = x
+union Empty y = y
+union (Entry k v next) y = insert k v $ union next y
+
+unions :: (Eq k, Foldable t) => t (Dict k v) -> Dict k v
+unions = foldr union Empty
+
+valid :: (Ord k) => Dict k v -> Bool
+valid d = valid' d Set.empty
+    where
+        valid' :: (Ord k) => Dict k v -> Set.Set k -> Bool
+        valid' Empty _ = True
+        valid' (Entry k _ next) set
+            | Set.member k set = False
+            | otherwise = valid' next $ Set.insert k set
